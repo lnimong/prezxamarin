fa727ef351a26388d0064e559f9f8592ffa46e2b
lnimong <lnimong@gmail.com>
2014-09-28 09:07:43 +0200

    android version

---
+++ exemples/AlloWorld/Android/DialService.cs
@@ -0,0 +1,26 @@
+﻿using AlloWorld.SharedCode;
+using Android.App;
+using Android.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Android
+{
+    public class DialService : IDialService
+    {
+        private Activity mainActivity;
+
+        public DialService(Activity mainActivity)
+        {
+            this.mainActivity = mainActivity;
+        }
+        public void Dial(string name, string number)
+        {
+            Intent callIntent = new Intent(Intent.ActionCall);
+            callIntent.SetData(Net.Uri.Parse("tel:" + number));
+            mainActivity.StartActivity(callIntent);
+        }
+    }
+}
---
+++ exemples/AlloWorld/Android/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using AlloWorld.SharedCode;
+using System.Linq;
+
+namespace Android
+{
+    [Activity(Label = "Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        private DialerModel model;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            model = new DialerModel(new DialService (this));
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            
+            
+            
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton0);
+
+            InitButton(button, model.Contacts.First(), model.Dial);
+
+        }
+
+        private void InitButton(Button button, string buttonname, Action<string> call)
+        {
+            button.Text = buttonname;
+            button.Click += delegate { call(buttonname); };
+        }
+    }
+}
+
